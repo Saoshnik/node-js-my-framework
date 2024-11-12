05586ece60c4e363ac6b490c5f3fa0657f20e16c
Saoshnik <kahockijandrzej@gmail.com>
2024-11-12 13:45:49 +0300

    install dotenv, add .env file,  launch node.js hello world server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+'use strict';
 
+const http = require('http');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const HOSTNAME = process.env.HOSTNAME;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+
+    res.end(JSON.stringify('Hello world'));
+});
+
+server.listen(PORT, HOSTNAME, () => console.log(`Server started on PORT ${PORT}`));
